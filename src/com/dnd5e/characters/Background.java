3aaaeb4c2d940ae932a52a8111d677e51daa4fd5
dither001 <32025687+dither001@users.noreply.github.com>
2019-02-07 12:52:29 -0700

    Background update
    Implemented Opposite/Similar interfaces

--- src/com/dnd5e/characters/Background.java
+++ src/com/dnd5e/characters/Background.java
@@ -3,7 +3,7 @@ package com.dnd5e.characters;
 import com.dnd5e.exceptions.*;
 import com.dnd5e.util.*;
 
-public enum Background {
+public enum Background implements Opposite, Similar {
 	ACOLYTE, CHARLATAN, CRIMINAL, ENTERTAINER, FOLK_HERO, GUILD_ARTISAN, HERMIT, NOBLE, OUTLANDER, SAGE, SAILOR, SOLDIER, URCHIN;
 
 	private static final Background[] BACKGROUNDS = { ACOLYTE, CHARLATAN, CRIMINAL, ENTERTAINER, FOLK_HERO,
@@ -14,6 +14,70 @@ public enum Background {
 	 */
 	public int indexOf() {
 		return Misc.indexOfEnum(this.toString(), BACKGROUNDS);
+	}
+
+	public boolean isClergy() {
+		return this.equals(ACOLYTE);
+	}
+
+	public boolean isNoble() {
+		return this.equals(NOBLE);
+	}
+
+	public boolean isOutlaw() {
+		return this.equals(CHARLATAN) || this.equals(CRIMINAL) || this.equals(HERMIT) || this.equals(OUTLANDER)
+				|| this.equals(URCHIN);
+	}
+
+	public boolean isTradesman() {
+		return this.equals(ENTERTAINER) || this.equals(FOLK_HERO) || this.equals(GUILD_ARTISAN) || this.equals(SAGE)
+				|| this.equals(SAILOR) || this.equals(SOLDIER);
+	}
+
+	@Override
+	public int similarTo(Object o) {
+		int similar = -1;
+
+		if (o.getClass().equals(Background.class)) {
+			Background b = (Background) o;
+
+			if (b.isClergy() && (this.isOutlaw() || this.isNoble()))
+				similar = 1;
+			else if (b.isOutlaw() && (this.isTradesman() || this.isClergy()))
+				similar = 1;
+			else if (b.isTradesman() && (this.isNoble() || this.isOutlaw()))
+				similar = 1;
+			else if (b.isNoble() && (this.isClergy() || this.isTradesman()))
+				similar = 1;
+
+			if (b.equals(this))
+				similar = 0;
+		}
+
+		return similar;
+	}
+
+	@Override
+	public boolean opposedTo(Object o) {
+		boolean opposed = false;
+
+		if (o.getClass().equals(Background.class)) {
+			Background b = (Background) o;
+
+			if (b.isClergy() && this.isTradesman())
+				opposed = true;
+			else if (b.isOutlaw() && this.isNoble())
+				opposed = true;
+			else if (b.isTradesman() && this.isClergy())
+				opposed = true;
+			else if (b.isNoble() && this.isOutlaw())
+				opposed = true;
+
+			if (b.equals(this))
+				opposed = false;
+		}
+
+		return opposed;
 	}
 
 	/*

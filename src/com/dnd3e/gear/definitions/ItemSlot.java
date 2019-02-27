76fbf617639310f34eaec3c745a84a1e2eae1f25
dither001 <32025687+dither001@users.noreply.github.com>
2019-02-27 14:20:04 -0700

    Initial commit

---
+++ src/com/dnd3e/gear/definitions/ItemSlot.java
@@ -0,0 +1,24 @@
+package com.dnd3e.gear.definitions;
+
+/*
+ * This class uses Magic of Incarnum as the basis for its limited body slots. 
+ * The 3e Dungeon Master's Guide was permissive with locations for potential 
+ * magic items, requiring DM Fiat for numerous corner cases.
+ */
+
+/*
+ * 4e attempted to curb the need for magic items by limiting the most useful 
+ * effects to three primary locations/types: weapon, "neck," and armor. 5e 
+ * curbed this further by abstracting magic items to "attunement slots," and 
+ * allowing 3 of any such type.
+ */
+
+public enum ItemSlot {
+	ARMS, BROW, CHARM, CHEST, CROWN, FEET, HANDS, LEGS, MANTLE, WAIST;
+
+	// upper: hands, arms, mantle
+	// lower: feet, legs, waist
+	// body: chest, brow, crown
+	// misc: charm (amulet, rings, accessory)
+
+}
